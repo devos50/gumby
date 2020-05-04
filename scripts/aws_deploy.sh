81cbfd1beddd246a783ac5d269c1925ce4e116d8
Martijn de Vos <mdmartijndevos@gmail.com>
2020-05-04 17:10:24 +0200

    Waiting for SSH processes

--- scripts/aws_deploy.sh
+++ scripts/aws_deploy.sh
@@ -4,7 +4,13 @@ while read -r SERVER
 do
   cmd="AWS_INSTANCES_TO_RUN=$AWS_INSTANCES_TO_RUN AWS_NODE_AMOUNT=$AWS_NODE_AMOUNT EXPERIMENT_NAME=$EXPERIMENT_NAME AWS_INSTANCE_COMMAND=$DAS4_NODE_COMMAND AWS_NODE_TIMEOUT=$AWS_NODE_TIMEOUT SYNC_PORT=$SYNC_PORT SCENARIO_FILE=$SCENARIO_FILE EXPERIMENT_DIR=/home/ec2-user/gumby/experiments/dummy VENV=/home/ec2-user/venv3 gumby/scripts/aws_run.sh"
   ssh -n ec2-user@$SERVER -i ~/Amazon.pem $cmd &
+  pids[${i}]=$!
 done < "$AWS_SERVERS_FILE"
+
+# wait for all pids
+for pid in ${pids[*]}; do
+    wait $pid
+done
 
 # Rsync everything back
 echo "RSynching results back"
