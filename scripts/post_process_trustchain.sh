69214f310f3350581144a3a58a6ee8d196eefd2b
Martijn de Vos <mdmartijndevos@gmail.com>
2020-07-01 14:27:16 +0200

    Parsing IPv8 stats after TrustChain experiment

--- scripts/post_process_trustchain.sh
+++ scripts/post_process_trustchain.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 gumby/experiments/trustchain/post_process_trustchain.py .
 
+# Parse IPv8 statistics
+gumby/experiments/ipv8/parse_ipv8_statistics.py .
+graph_ipv8_stats.sh
+
 # Run the regular statistics extraction script
 graph_process_guard_data.sh
